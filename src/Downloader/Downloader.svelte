55c771cc5d9efeeae3cc725125a6365c33db001a
Andrea055o <vivanco11.ac@gmail.com>
2022-05-26 22:29:32 +0200

    remove console.log

--- src/Downloader/Downloader.svelte
+++ src/Downloader/Downloader.svelte
@@ -2,17 +2,14 @@
     import { invoke } from '@tauri-apps/api/tauri';
     import { save } from '@tauri-apps/api/dialog';
     import { dialogs } from 'svelte-dialogs';
-    export let current_path: string, url: string, files: Array<string>;
+    export let url: string, files: Array<string>;
     async function sync(){
-        console.log(current_path);
         let folder_download: any = await save();
         if(folder_download != undefined){
             folder_download = folder_download.split("/");
             folder_download.pop();
             files.forEach(async(file) => {
-                console.log(url + "/" + file.file);
                 let response = await invoke('download', {url: url + "/" + file.file, path: file.file, savefolder: folder_download.join('/')});
-                console.log(response);
             })
         }else{
             dialogs.alert("Please select a correct folder to download");
